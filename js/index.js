f72644b93687f96972986dda78338be58b488e13
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-07 21:08:10 -0300

    button clear and result

--- js/index.js
+++ js/index.js
@@ -5,11 +5,12 @@ const inputPeso = document.getElementById('peso')
 
 // button
 
-const buttonCalculo = document.querySelector('.botao')
+const buttonCalculo = document.querySelector('#calcular')
+const buttonLimpar = document.querySelector('#limpar')
 
 // result
 
-const resultado = document.querySelector('.resultado')
+const textResultado = document.querySelector('.resultado')
 
 // function calculo
 
@@ -18,6 +19,14 @@ buttonCalculo.addEventListener('click', (e) => {
 
     const altura = parseFloat(inputAltura.value)
     const peso = parseFloat(inputPeso.value)
+    const resultado = peso / (Math.pow(altura,2))
 
-    resultado.innerHTML += parseInt(Math.pow(peso, 2))
+    textResultado.innerHTML += `${resultado.toFixed(2)}`
+
+})
+
+buttonLimpar.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    textResultado.innerHTML = ''
 })
